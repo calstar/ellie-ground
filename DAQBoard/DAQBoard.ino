b0030dc6bf8e8ddca4f999868dc5fd73bc1b2840
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2022-04-02 18:15:42 -0700

    Apr 2 Edition
    First upload to ellie-ground project

---
+++ DAQBoard/DAQBoard.ino
@@ -0,0 +1,274 @@
+/*
+This code runs on the DAQ ESP32 and has a couple of main functions.
+1. Read sensor data
+2. Send sensor data to COM ESP32
+3. Recieve servo commands from COM ESP32
+4. Send PWM signals to servos
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <ESP32Servo.h>
+#include <Wire.h>
+#include <Arduino.h>
+#include "HX711.h"
+
+//define pins to use for the various sensors and connetcions. define takes up less space on the chip
+#define ONBOARD_LED  12
+#define PT1DOUT 26
+#define PT2DOUT 44
+
+#define CLK 25
+#define FM 4
+
+#define S1S 21
+
+//define servo min and max values
+#define SERVO_MIN_USEC (900)
+#define SERVO_MAX_USEC (2100)
+
+//Initialize flow meter variables for how it computes the flow ammount
+float currentMillis = 0;
+float goalTime = 100;
+float currReading1;
+float currReading2;
+
+
+//FM counter
+float fmcount;
+float flowRate;
+boolean currentState;
+boolean lastState = false;
+
+
+//Initialize the PT and LC sensor objects which use the HX711 breakout board
+HX711 scale1;
+HX711 scale2;
+
+//Initialize the servo objects
+Servo servo1;
+Servo servo2;
+
+//define servo necessary values
+int ADC_Max = 4096;
+
+
+///////////////
+//IMPORTANT
+//////////////
+// REPLACE WITH THE MAC Address of your receiver
+// uint8_t broadcastAddress[] = {0xC4, 0xDD, 0x57, 0x9E, 0x91, 0x6C};
+// Hubert receiver
+uint8_t broadcastAddress[] = {0x40, 0xF5, 0x20, 0x44, 0xB3, 0xC4};
+
+
+// Define variables to store readings to be sent
+float pt1;
+float pt2;
+float pt3;
+float pt4;
+float pt5;
+float lc1;
+float lc2;
+float lc3;
+float fm;
+//the following are only used in the oposite diretcion, they are included because it may be necessary for the structure to be the same in both directions
+int S1; int S2; int S1S2; int I;
+
+
+
+
+// Define variables to store incoming commands, servos and igniter
+int incomingS1;
+int incomingS2;
+int incomingS1S2;
+bool incomingI;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float pt1;
+    float pt2;
+    float pt3;
+    float pt4;
+    float pt5;
+    float lc1;
+    float lc2;
+    float lc3;
+    float fm;
+
+    int S1;
+    int S2;
+    int S1S2;
+    bool I;
+} struct_message;
+
+// Create a struct_message called Readings to hold sensor readings
+struct_message Readings;
+
+// Create a struct_message to hold incoming commands
+struct_message Commands;
+
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&Commands, incomingData, sizeof(Commands));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  S1 =Commands.S1;
+  S2 = Commands.S2;
+  S1S2 = Commands.S1S2;
+  I = Commands.I;
+
+}
+
+
+void setup() {
+  //attach servo pins
+  servo1.attach(S1S,SERVO_MIN_USEC,SERVO_MAX_USEC );
+
+  // attach onboard LED
+  pinMode(ONBOARD_LED,OUTPUT);
+
+
+//attach flowmeter pin
+  pinMode(FM, INPUT_PULLUP);
+
+//set gains for pt pins
+  scale1.begin(PT1DOUT, CLK);
+  scale1.set_gain(64);
+  scale2.begin(PT2DOUT, CLK);
+  scale2.set_gain(64);
+
+  Serial.begin(115200);
+
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+
+
+void loop() {
+  //Set LED back to low
+    digitalWrite(ONBOARD_LED,LOW);
+
+
+
+ //ADD PRINT STATEMENTS FOR DEBUGGING HERE IF NCESSARY
+ // printSerial();
+
+
+//UPDATE SERVO POSITIONS
+  //Check new data for servo status updates
+  switch (S1) {
+    case 0:
+        servo1.write(0);
+        break;
+    case 90:
+        servo1.write(90);
+        break;
+    case 130:
+        servo1.write(135);
+        break;
+  }
+
+
+
+
+  getReadings();
+
+
+  // Set values to send
+  Readings.pt1 = pt1;
+  Readings.pt2 = pt2;
+  Readings.pt3 = pt3;
+  Readings.pt4 = pt4;
+  Readings.pt5 = pt5;
+  Readings.lc1 = lc1;
+  Readings.lc2 = lc2;
+  Readings.lc3 = lc3;
+  Readings.fm  = fm;
+  //micmic behavior of checking servo angle
+  //for now this just takes the value from the sender. but once we are able to check the servo angle, switch out S1 to servo value
+  Readings.S1 = S1;
+// send the servo degree back
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Readings, sizeof(Readings));
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+    digitalWrite(ONBOARD_LED,HIGH);
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(50);
+}
+
+
+
+void getReadings(){
+  currentMillis = millis();
+  if (goalTime < currentMillis) {
+
+    goalTime = currentMillis + 50;
+    flowRate=fmcount;
+    fmcount=0;
+  }
+
+    currentState = digitalRead(FM);
+    if (!(currentState == lastState)) {
+      lastState = currentState;
+      fmcount+=1;
+    }
+
+  fm =int(flowRate*10000+1);  // Print the integer part of the variable
+
+   pt1 = scale1.read();
+
+   pt2 = scale2.read();
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ellie-ground
 2022 Eillie Hot-firing Electronics control
+
+## Summary
+**DAQboard ino** file commands the ESP32 that reads data from sensors and commands from the COM Board, and sends data in a package to the COM Board
+**COMBoard ino** file commands the ESP32 that receives data in a package from the DAQ Board, sends commands to the DAQ Board, and sends data to MATLAB for plotting and data-saving purposes through the serial port.
+**liveplot.m** file is a MATLAB file that can receive data from the COM Board, create live plots, and save the data.
+
+## Software requirement
+1. Arduino (https://www.arduino.cc/en/software)
+2. MATLAB (https://software.berkeley.edu/matlab®)
+  - To control the serial port, you need to install the **Instrument Control Toolbox**. Link: https://www.mathworks.com/products/instrument.html
+3. (optional) An external code editing software such as Atom (https://atom.io) or Visual Studio (https://visualstudio.microsoft.com/downloads/).
+  - Arduino IDE and MATLAB are sufficient for all the coding
+4. (recommended) GitHub Desktop (https://desktop.github.com). You can pull, push, and edit files with ease. **Step 3 required if you would like to download GitHub Desktop as the software requires an external editor.**
+
+## How to update
+### Using Github Desktop
+1. Make sure you have access to this project
+  - Check with you administrators
+2. Go to the Github app, sign in, and click on the top left corner. You should see a pop up window with an "Add" button. Select that button and choose "Clone repository"
+3. Search in "Filter you repositories" using keyword "ellie-ground." Select the correct repository and click "Clone."
+  - If you want this cloned repository to be in a different folder, change the "Local Path."
+4. After you edit the file in the local repository, GitHub Desktop will detect changes and prompt you to commit. Make sure to double check your changes before committing.
+5. On the bottom left corner, put down a short summary that describes what you have changed, then click "Commit to *main*"
+6. Check the project online and see if the changes have been reflected.
+
+### Using Terminal
+(I don't know the technical details so if anyone does please add on)
