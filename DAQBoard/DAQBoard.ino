7839cd9de83ff867efd8fdbad404a527c5cbbfa3
tzinky5 <tzinky5@gmail.com>
2022-04-04 21:38:00 -0700

    Revert "Add files via upload"
    This reverts commit e99c1198ff01b9ae491ee7c082d96d64acfec7d3.

--- DAQBoard/DAQBoard.ino
+++ DAQBoard/DAQBoard.ino
@@ -3,17 +3,17 @@ This code runs on the DAQ ESP32 and has a couple of main functions.
 1. Read sensor data
 2. Send sensor data to COM ESP32
 3. Recieve servo commands from COM ESP32
-4. Send PWM signals to servos 
+4. Send PWM signals to servos
 */
 
 #include <esp_now.h>
 #include <WiFi.h>
-#include <ESP32Servo.h> 
+#include <ESP32Servo.h>
 #include <Wire.h>
 #include <Arduino.h>
 #include "HX711.h"
 
-//define pins to use for the various sensors and connetcions. define takes up less space on the chip 
+//define pins to use for the various sensors and connetcions. define takes up less space on the chip
 #define ONBOARD_LED  12
 #define PT1DOUT 26
 #define PT2DOUT 16
@@ -29,10 +29,10 @@ This code runs on the DAQ ESP32 and has a couple of main functions.
 
 //Initialize flow meter variables for how it computes the flow ammount
 float currentMillis = 0;
-float goalTime = 100; 
+float goalTime = 100;        // [ms]
 float currReading1;
 float currReading2;
-float loopTime=100;
+
 
 //FM counter
 float fmcount;
@@ -50,28 +50,26 @@ Servo servo1;
 Servo servo2;
 
 //define servo necessary values
-int ADC_Max = 4096;   
+int ADC_Max = 4096;
 
 
 ///////////////
 //IMPORTANT
 //////////////
-// REPLACE WITH THE MAC Address of your receiver 
+// REPLACE WITH THE MAC Address of your receiver
 uint8_t broadcastAddress[] = {0xC4, 0xDD, 0x57, 0x9E, 0x91, 0x6C};
-
-int count=3;
 
 
 // Define variables to store readings to be sent
-float pt1=1;
-float pt2=1;
-float pt3=1;
-float pt4=1;
-float pt5=1;
-float lc1=1;
-float lc2=1;
-float lc3=1;
-float fm=2;
+float pt1;
+float pt2;
+float pt3;
+float pt4;
+float pt5;
+float lc1;
+float lc2;
+float lc3;
+float fm;
 //the following are only used in the oposite diretcion, they are included because it may be necessary for the structure to be the same in both directions
 int S1; int S2; int S1S2; int I;
 
@@ -83,11 +81,6 @@ int incomingS1;
 int incomingS2;
 int incomingS1S2;
 bool incomingI;
-
-
-float startTime;
-float endTime;
-float timeDiff;
 
 // Variable to store if sending data was successful
 String success;
@@ -122,48 +115,45 @@ esp_now_peer_info_t peerInfo;
 
 // Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // Serial.print("\r\nLast Packet Send Status:\t");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   if (status ==0){
     success = "Delivery Success :)";
   }
   else{
-    success = "Delivery Fail :(";
+    // success = "Delivery Fail :(";
   }
 }
 
 // Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&Commands, incomingData, sizeof(Commands));
-  Serial.print("Bytes received: ");
+  // Serial.print("Bytes received: ");
   Serial.println(len);
   S1 =Commands.S1;
   S2 = Commands.S2;
   S1S2 = Commands.S1S2;
   I = Commands.I;
-  
+  //Serial.print("IT IS WORKING");
 }
 
- 
+
 void setup() {
   //attach servo pins
   servo1.attach(S1S,SERVO_MIN_USEC,SERVO_MAX_USEC );
-  
+
   // attach onboard LED
   pinMode(ONBOARD_LED,OUTPUT);
 
 
 //attach flowmeter pin
-  //pinMode(FM, INPUT_PULLUP);
+  pinMode(FM, INPUT_PULLUP);
 
 //set gains for pt pins
   scale1.begin(PT1DOUT, CLK);
   scale1.set_gain(64);
   scale2.begin(PT2DOUT, CLK);
   scale2.set_gain(64);
-//Flowmeter untreupt 
- pinMode(FM, INPUT);           //Sets the pin as an input
- attachInterrupt(FM, Flow, RISING); 
 
   Serial.begin(115200);
 
@@ -180,13 +170,13 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
-  
+
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  
-  // Add peer        
+
+  // Add peer
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Failed to add peer");
     return;
@@ -195,39 +185,40 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-  
- 
+
+
 void loop() {
-  startTime=millis();
-  //Set LED back to low 
+  //Set LED back to low
     digitalWrite(ONBOARD_LED,LOW);
- 
+
 
 
  //ADD PRINT STATEMENTS FOR DEBUGGING HERE IF NCESSARY
  // printSerial();
-  
+
 
 //UPDATE SERVO POSITIONS
   //Check new data for servo status updates
-  switch (S1) {
-    case 0:
-        servo1.write(0);
-        break;
-     case 45:
-        servo1.write(45);
-        break;       
-    case 90:
-        servo1.write(90);
-        break;
-    case 130:
-        servo1.write(135);
-        break;
-  }
+
+  //switch (S1) {
+    //case 0:
+        //servo1.write(0);
+        //break;
+    //case 45:
+        //servo1.write(45);
+        //break;
+    //case 90:
+        //servo1.write(90);
+        //break;
+    //case 130:
+        //servo1.write(135);
+        //break;
+    servo1.write(S1);
+  //}
 
   getReadings();
 
- 
+
   // Set values to send
   Readings.pt1 = pt1;
   Readings.pt2 = pt2;
@@ -241,45 +232,51 @@ void loop() {
 
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Readings, sizeof(Readings));
-   
+
   if (result == ESP_OK) {
-    Serial.println("Sent with success");
+    // Serial.println("Sent with success");
     digitalWrite(ONBOARD_LED,HIGH);
   }
   else {
     Serial.println("Error sending the data");
   }
-
-  endTime=millis();
-  timeDiff=endTime-startTime;
-//  if (timeDiff<loopTime) {
-//    delay(timeDiff);
-//  }
-  
+  delay(50);
 }
 
 
 
 void getReadings(){
   currentMillis = millis();
- // if (goalTime < currentMillis) {
- //   
- //   goalTime = currentMillis + 50;
- //   flowRate=fmcount;
- //   fmcount=0;
- // }
+  fmcount = 0;
+  while (millis() - currentMillis < goalTime) {
+    currentState = digitalRead(FM);
+    if (!(currentState == lastState)) {
+      lastState = currentState;
+      fmcount += 1;
+    }
+  }
+  flowRate = fmcount * 1000 / goalTime;
+  //if (goalTime < currentMillis) {
 
-  
-  
-  fm =count;  // Print the integer part of the variable
-  count=4;
+    //goalTime = currentMillis + 50;
+    //flowRate=fmcount;
+    //fmcount=0;
+  //}
+
+    //currentState = digitalRead(FM);
+    //if (!(currentState == lastState)) {
+      //lastState = currentState;
+      //fmcount+=1;
+    //}
+
+  fm =int(flowRate*10000+1);  // Print the integer part of the variable
+
    pt1 = scale1.read();
-   
+
    pt2 = scale2.read();
 
-}
+   Serial.print(String(pt1)+" ");
+   Serial.println(String(pt2));
 
-void Flow()
-{
-   count++; //Every time this function is called, increment "count" by 1
 }
+// hi! How r u?
