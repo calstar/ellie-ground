f78e399c54199006f8f191e7d6ac21bdc28e5e27
Sharonol <sharononofre.14@berkeley.edu>
2022-04-06 01:34:51 -0700

    Read Table

--- MATLAB_Plotting/readTable.m
+++ MATLAB_Plotting/readTable.m
@@ -1,4 +1,7 @@
 clear all
 
-fileName = "blah"
-readtable(fileName)
+fileName = "Test_Data_2022-04-05/2022-04-05_234516_fm_test1.xls"
+T = readtable(fileName)
+A = table2array(T)
+C4 = T{:,4}
+NotOnes = C4(C4 ~= 1)
