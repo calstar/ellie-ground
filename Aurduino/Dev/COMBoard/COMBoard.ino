6ec6dc27ee1c79781388dd000f27a55bd97bd745
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2022-05-16 21:57:51 -0700

    update

--- Aurduino/Dev/COMBoard/COMBoard.ino
+++ Aurduino/Dev/COMBoard/COMBoard.ino
@@ -99,7 +99,7 @@ struct_message Commands;
 //
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
  // Serial.print("\r\nLast Packet Send Status:\t");
- // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+ Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   if (status == 0){
     success = "Delivery Success :)";
     digitalWrite(DAQIndicator, HIGH);
--- Aurduino/Dev/DAQBoard/DAQBoard.ino
+++ Aurduino/Dev/DAQBoard/DAQBoard.ino
@@ -173,13 +173,13 @@ esp_now_peer_info_t peerInfo;
 // Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
-  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-//   if (status ==0){
-//     success = "Delivery Success :)";
-//   }
-//   else{
-//     success = "Delivery Fail :(";
-//   }
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // if (status ==0){
+  //   success = "Delivery Success :)";
+  // }
+  // else{
+  //   success = "Delivery Fail :(";
+  // }
 }
 
 // Callback when data is received
