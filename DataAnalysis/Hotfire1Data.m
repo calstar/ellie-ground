3c1efeae1d8b5fd9e14227789625fd090be89ac5
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2022-06-02 16:02:05 -0400

    Processed Hot Fire Data

--- DataAnalysis/Hotfire1Data.m
+++ DataAnalysis/Hotfire1Data.m
@@ -1,32 +1,64 @@
-fileName='HotfireData2'
+clear;
+close all
+fileName='HotfireData2';
 table = readtable([fileName,'.xlsx']);
 dataArray = table2array(table);
+deletePreIndex = 1690; % There is a break in the data sheet, check index 1590 for Hotfire2Data
+deletePostIndex = 250;
+dataArray(1:deletePreIndex,:) = [];
+dataArray(deletePostIndex:end,:) = [];
+saveFileName = fileName+"_Processed";
 
-close all
+dataTypes = ["double","double","double","double","double","double","double","double","double","double"];
+dataLabels = ["time (s)","PT1 (psi)","PT2 (psi)","PT3 (psi)","PT4 (psi)","LC5 (lbs)","LC6 (lbs)","LC7 (lbs)","FM","S1","S2","commandedState","DAQState","Queue Size"];
+ylabels = ["seconds","psi","psi","psi","psi","lb","lb","lb"];
+% sz = [1,dataLength];
 
-dataArray
+const = [0,0;
+    1.1293e-04,12.4897;
+    2.7716e-05,17.5329;
+    1.1438e-04,16.2320;
+    2.7673e-05,18.9369;
+    -3.0272e-04,-0.1535;
+    -3.4136e-04,0.8333;
+    -2.4978e-04,1.6369];
+
+% const = [ones(8,1),zeros(8,1)];
+
+maxPoints = [NaN,NaN];
+minPoints = [NaN,NaN];
+dataArray(:,1) = (dataArray(:,1)-dataArray(1,1))/1000;
+for i = 2:length(const)
+dataArray(:,i) = dataArray(:,i).*const(i,1)+const(i,2);
+[minPoint,minIndex] = min(dataArray(:,i));
+[maxPoint,maxIndex] = max(dataArray(:,i));
+maxPoints = [maxPoints;maxPoint,maxIndex]; 
+minPoints = [minPoints;minPoint,minIndex]; 
+end
+
+maxThrust = -sum(minPoints(6:8,1)-maxPoints(6:8,1))
+
+table = array2table(dataArray,"VariableNames",dataLabels);
 figure
-plot(dataArray(:,1),dataArray(:,2))
 
-figure
-plot(dataArray(:,1),dataArray(:,3))
-
-figure
-plot(dataArray(:,1),dataArray(:,4))
-
-figure
-plot(dataArray(:,1),dataArray(:,5))
-
-
-figure
-plot(dataArray(:,1),dataArray(:,6))
-
-
-figure
-plot(dataArray(:,1),dataArray(:,7))
+% [~,col] = size(dataArray);
+for k = 2:8
+nexttile;
+p = plot(dataArray(:,1),dataArray(:,k));
+dtmax = datatip(p,dataArray(maxPoints(k,2),1),maxPoints(k,1));
+if k >= 6
+    dtmin = datatip(p,dataArray(minPoints(k,2),1),minPoints(k,1));
+end
+title(dataLabels(k));
+xlabel(ylabels(1));
+ylabel(ylabels(k));
+% nexttile;
+end
 
 
-figure
-plot(dataArray(:,1),dataArray(:,8))
+writetable(table,saveFileName,"FileType","spreadsheet");
+% fileString = saveFileName + ".xls";
+% movefile(fileString,folderName);
+
 
 
